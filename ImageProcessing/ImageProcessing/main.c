dadf34525f0892c607096a931f9482e6cba71f43
huyc0928 <huyunchen@mail.ustc.edu.cn>
2025-11-26 20:59:35 +0800

    Add File

---
+++ ImageProcessing/ImageProcessing/main.c
@@ -0,0 +1,4 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "D:\USTC\Program\ImageProcessing\stb_image.h"
+#include "D:\USTC\Program\ImageProcessing\stb_image_write.h"
